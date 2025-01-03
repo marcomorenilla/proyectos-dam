39c99639d6ff7c8e4966812d75db26f7c4f2f7de
Marco Antonio Morenilla Alonso <marcomorenilla@gmail.com>
2025-01-03 15:49:49 +0100

    Update UF2
    añadimos archivos a UF2

--- DI/UF2/src/other-dialogs.py
+++ DI/UF2/src/other-dialogs.py
@@ -3,7 +3,8 @@
 # Utilizamos la librería PySide6
 # Autor: Marco Antonio Morenilla Alonso
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QHBoxLayout, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QHBoxLayout, QVBoxLayout, QWidget, QFileDialog, QColorDialog, QFontDialog, QInputDialog
+
 
 class OtherDialogs (QMainWindow):
     def __init__(self):
@@ -24,14 +25,23 @@ class OtherDialogs (QMainWindow):
         layout.addLayout(row_2)
 
 
-        # Creamos botones
+        # Creamos botones fila 1
         button_open = QPushButton('Abrir archivo')
         button_save =  QPushButton('Guardar archivo')
-        button_color =  QPushButton('Elige color')
+        button_color =  QPushButton('Elección de color')
 
         button_open.clicked.connect(self.open_file)
         button_save.clicked.connect(self.save_file)
         button_color.clicked.connect(self.color_selection)
+
+        # Creamos botones fila 2
+        button_font =  QPushButton('Selección de fuente')
+        button_input =  QPushButton('Introducción de datos')
+        
+
+        button_font.clicked.connect(self.font_selection)
+        button_input.clicked.connect(self.input_window)
+        
 
 
         # Añadimos botones a layouts
@@ -39,6 +49,10 @@ class OtherDialogs (QMainWindow):
         row_1.addWidget(button_open)
         row_1.addWidget(button_save)
         row_1.addWidget(button_color)
+
+        row_2.addWidget(button_font)
+        row_2.addWidget(button_input)
+        
 
         # Conectamos QWidget con layouts y lo añadimos a QMainWindow
         container.setLayout(layout)
@@ -49,11 +63,60 @@ class OtherDialogs (QMainWindow):
     def open_file(self):
         print('código para abrir archivo')
 
+        # Creamos un QFileDialog y accedemos al método getOpenFileName
+        # Devuelve una tupla con los archivos seleccionados y accedemos a ellos por su posición
+
+        dialog = QFileDialog.getOpenFileName(self,caption='Abrir archivo...',
+        dir='.',
+        filter='Texto (*.txt);; PDF (*.pdf)',
+        selectedFilter='PDF (*.pdf)')
+
+        file_1 = dialog[0]
+        # A partir de aquí trabajamos con el archivo
+
     def save_file(self):
         print('Código para salvar archivo')
 
+        #Creamos un QFileDialog y accedemos al método getSaveFileName
+
+        dialog = QFileDialog.getSaveFileName(self, caption='Guardar archivo...',
+        dir='.',
+        filter='Texto (*.txt);;PDF (*.pdf)',
+        selectedFilter='PDF (*.pdf)')
+
+        file_1 = dialog[0]
+        print(file_1)
+        
+
     def color_selection(self):
         print('Código para seleccionar color')
+    
+        color = QColorDialog.getColor()
+        print(color)
+
+        # TODO Averiguar como asignar color a botón
+    
+    def font_selection(self):
+        print('Códio para selección de fuente')
+
+        font =  QFontDialog.getFont()
+        print(font)
+
+        # TODO Averiguar como asignar fuente a botón
+
+    
+    def input_window(self):
+        print('Código para introducción de datos')
+        # Devuelve  una tupla[str, bool], el str se asigna a mes y el bool a selection
+
+        month, selection = QInputDialog.getItem(self,'Meses', 'Meses',
+        ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'])
+
+        if selection:
+            print(month)
+
+   
+        
 
 if __name__ == '__main__':
     app = QApplication([])
---
+++ DI/UF2/src/thread/thread.py
@@ -0,0 +1,70 @@
+__package__
+
+# Trabajamos componentes de UI de la UF2
+# Archivo con descripciones ventana.py
+# Utilizamos la librería PySide6
+# Autor: Marco Antonio Morenilla Alonso
+
+from PySide6.QtCore import QThread, Signal
+from PySide6.QtWidgets import QMainWindow,  QPushButton, QProgressDialog, QApplication, QVBoxLayout
+
+
+class WorkerThread(QThread):
+    progress = Signal(int)
+    finished = Signal()
+
+    def run(self):
+        for i in range(11):
+            self.sleep(1)  # Simulate work
+            self.progress.emit(i)
+        self.finished.emit()
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Copia')
+
+        self.button_copy = QPushButton('Click para iniciar')
+
+        self.button_copy.clicked.connect(self.start_thread)
+        
+        self.setCentralWidget(self.button_copy)
+        
+
+    def start_thread(self):
+        self.button_copy.setEnabled(False)
+
+        self.progress = QProgressDialog('Copiando archivos', 'Cancelar', 0, 10, self)
+        self.progress.canceled.connect(self.cancel_thread)
+
+        self.worker = WorkerThread()
+        self.worker.progress.connect(self.update_progress)
+        self.worker.finished.connect(self.thread_finished)
+
+        self.worker.start()
+        self.progress.show()
+
+    def update_progress(self, progreso):
+        self.progress.setValue(progreso)
+
+    def thread_finished(self):
+        self.progress.close()
+        self.button_copy.setEnabled(True)
+
+    def cancel_thread(self):
+        self.worker.terminate()
+        self.progress.close()
+        self.button_copy.setEnabled(True)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec()
+
+
+
+   
+
