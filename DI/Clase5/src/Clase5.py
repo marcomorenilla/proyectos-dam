70d012857ccaa4af80a6125c2738c8dc00de87e4
Marco Antonio Morenilla Alonso <marcomorenilla@gmail.com>
2025-02-07 18:41:39 +0100

    Update
    Añadimos funconalidades a la app y menú ayuda.

--- DI/Clase5/src/Clase5.py
+++ DI/Clase5/src/Clase5.py
@@ -1,9 +1,10 @@
 import sys
+from tkinter.commondialog import Dialog
 
 from PySide6.QtCore import QFile, Qt
-from PySide6.QtGui import QAction
+from PySide6.QtGui import QAction, QKeySequence
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QMainWindow, QApplication, QMenu
+from PySide6.QtWidgets import QMainWindow, QApplication, QMenu, QInputDialog, QMessageBox
 
 
 class MainWindow(QMainWindow):
@@ -34,10 +35,37 @@ class MainWindow(QMainWindow):
         self.ui.plainTextEdit.setContextMenuPolicy(Qt.CustomContextMenu)
         self.ui.plainTextEdit.customContextMenuRequested.connect(self.mostrar_menu_contextual)
 
+        ## UNIR ACCIÓN COPIAR CON DEF COPIAR
+        self.ui.actionCopiar.setShortcut(QKeySequence("Ctrl+c"))
+        self.ui.actionCopiar.triggered.connect(self.copiar)
+
+        ## UNIR ACCION PEGAR CON DEF PEGAR
+        self.ui.actionPegar.setShortcut(QKeySequence("Ctrl+v"))
+        self.ui.actionPegar.triggered.connect(self.pegar)
+
+        ## UNIR ACCIÓN LIMPIAR CON DEF LIMPIAR
+        self.ui.actionLimpiar.setShortcut(QKeySequence("Ctrl+l"))
+        self.ui.actionLimpiar.triggered.connect(self.limpiar)
+
+        ## UNIR ACCIÓN MOSTRAR MENSAJE PERSONALIZADO
+        self.ui.actionMensaje.triggered.connect(self.mensaje_personalizado)
+
+        ## Cargamos widget de ayuda
+        loader_ayuda = QUiLoader()
+        self.ui_ayuda = loader_ayuda.load("../ui/ayuda.ui")
+
+        ## Menú ayuda
+        menu_ayuda = QMenu("Ayuda",self)
+        action_ayuda = QAction("Ayuda",self)
+        menu_ayuda.addAction(action_ayuda)
+        action_ayuda.triggered.connect(self.ayuda)
+
+        self.ui.menubar.addMenu(menu_ayuda)
+
+
     def cambiarTexto(self):
         texto = self.ui.plainTextEdit.toPlainText()
         self.ui.lblCambiar.setText(texto)
-
 
     def mostrar_menu_contextual(self, pos):
         menu_contextual = QMenu(self)
@@ -57,8 +85,22 @@ class MainWindow(QMainWindow):
 
     def pegar(self):
         self.ui.plainTextEdit.paste()
+
     def limpiar(self):
         self.ui.plainTextEdit.clear()
+
+    def mensaje_personalizado(self):
+        input, ok = QInputDialog().getText(self, 'Mensaje personalizado', 'Introduce un mensaje personalizado')
+        if input and ok:
+            QMessageBox.information(self, 'Mensaje personalizado', f'Mensaje personalizado: {str(input)} ')
+
+    def ayuda(self):
+        self.ui_ayuda.show()
+        self.ui_ayuda.btnCerrar.clicked.connect(self.cerrar_ayuda)
+
+    def cerrar_ayuda(self):
+        self.ui_ayuda.hide()
+
 
 if __name__ == "__main__":
     app = QApplication([])
