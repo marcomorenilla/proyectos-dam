e5a16ee11100fc16da37e3f455fd2e74a62453d0
Marco Antonio Morenilla Alonso <167623114+marcomorenilla@users.noreply.github.com>
2025-02-12 18:00:26 +0100

    Delete DI/Experiencial2/Connection.py
    Eliminamos datos de conexión subidos por error

--- DI/Experiencial2/Connection.py
+++
@@ -1,4 +0,0 @@
-# Configuración de la conexión a la base de datos Oracle
-DB_USER = "SYS"
-DB_PASSWORD = "vmware-db"  # Se recomienda cambiar esta contraseña por seguridad
-DB_DSN = "192.168.1.105/XEPDB1"  # Dirección de la base de datos
